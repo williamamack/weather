9729de697040ff24c259d9f9c6d398a9176111a0
Will Mack <williamamack@gmail.com>
2023-01-06 00:32:59 -0500

    Add API call to JS

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 import './style.css'
+import { getWeather } from './weather'
+
+getWeather(10,10, Intl.DateTimeFormat().resolvedOptions().timeZone)
+.then(res => console.log(res.data)
+
+)
--- weather.js
+++ weather.js
@@ -1,3 +1,14 @@
+// https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FNew_York
+
+import axios from "axios"
+
 export function getWeather(lat, lon, timezone){
-    
+ return axios.get("https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime",  
+ {
+  params: { 
+  latitude: lat,
+ longitude: lon,
+ timezone, 
+},
+})
 }
